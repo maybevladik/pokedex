546b8e620fa4013db323fb03d556aaf329213ae4
Vlad Bilyi <vladbilyip33@gmail.com>
2022-07-27 16:02:37 +0300

    First commit

---
+++ build/precache-manifest.4dca2e7405af7181a0465e59a457dac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6dd75f55d6d54306407429b6c18a96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de5fd5f5557c36c3662d",
+    "url": "/static/css/main.3ea7e1d3.chunk.css"
+  },
+  {
+    "revision": "ba3c274f096410c720f7",
+    "url": "/static/js/2.91ed52bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91ed52bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5fd5f5557c36c3662d",
+    "url": "/static/js/main.17c71e49.chunk.js"
+  },
+  {
+    "revision": "784d7b24f10a7a2fa5c1",
+    "url": "/static/js/runtime-main.350da645.js"
+  }
+]);
--- src/Layout/NavBar.js
+++ src/Layout/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Search from '../../src/Component/Search'
-
+ 
 function Navbar(){
     return(
         <div className='wrapper-navbar' >
             <div>
                 <p className='navbar-text' >Pokemons</p>
             </div>
-                
+
                 <Search />
                 
         </div>
